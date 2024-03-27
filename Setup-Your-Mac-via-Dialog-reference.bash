d76448ca62e1fe66f1051e0ea10eade3fb912091
MoyesSage <164223170+MoyesSage@users.noreply.github.com>
2024-03-27 10:41:52 -0600

    Testing commits

--- Setup-Your-Mac-via-Dialog-reference.bash
+++ Setup-Your-Mac-via-Dialog-reference.bash
@@ -341,7 +341,7 @@ jamfBinary="/usr/local/bin/jamf"
 
 welcomeTitle="Welcome to your new Mac, ${loggedInUserFirstname}!"
 welcomeMessage="select your preferred **Configuration** then click **Continue** to start applying settings to your new Mac.  \n\nOnce completed, the **Wait** button will be enabled and you'll be able to review the results before restarting your Mac.  \n\nIf you need assistance, please contact the USOPC Service Desk: +1(719)-866-4357.  \n\n---  \n\n#### Configurations  \n- **Required:** Minimum organization apps  \n- **Recommended:** Required apps and Microsoft Office  \n- **Complete:** Recommended apps, Adobe Acrobat Reader and Google Chrome"
-welcomeBannerImage="https://github.com/ModicaRobinsonA/Set-Up-My-Mac/blob/0aca34ff2abe1e2d28f66eb3f2e4edfad8e3dc52/Pictures/Soft%20Stars.jpeg"
+welcomeBannerImage="https://raw.githubusercontent.com/ModicaRobinsonA/Set-Up-My-Mac/main/Pictures/Soft%20Stars.jpeg?token=GHSAT0AAAAAACP3YFUI6U5F3Q2TFG4ERAISZQEJMUQ"
 welcomeBannerText="Welcome to your new Mac, ${loggedInUserFirstname}!"
 welcomeCaption="Please review the above video, then click Continue."
 welcomeVideoID="vimeoid=803933536"
